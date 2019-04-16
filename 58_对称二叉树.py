27558e3e4adb7fbc29dfc5957169d17dff3f6025
Weikoi <huangzengrui@yahoo.com>
2019-04-16 14:01:31 +0800

    modify

--- 58_对称二叉树.py
+++ 58_对称二叉树.py
@@ -1,19 +1,16 @@
 class Solution:
     def isSymmetrical(self, root):
         # write code here
-        if root is None:
+        if not root:
             return True
         else:
-            return (self.check(root.left, root.right))
+            return self.check(root.left, root.right)
 
     def check(self, left, right):
-        if left is None and right is None:
+        if not left and not right:
             return True
-        if left is None or right is None:
+        if not left or not right:
             return False
-        if left.val != right.val:
+        if left.val!= right.val:
             return False
-        if self.check(left.left, right.right) and self.check(left.right, right.left):
-            return True
-        else:
-            return False
+        return self.check(left.left, right.right) and self.check(left.right, right.left)
